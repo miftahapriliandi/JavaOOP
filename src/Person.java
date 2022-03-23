078506d91d0cd45065bb3f987799593f7c25b44b
miftahapriliandi <miftahapriliandi@gmail.com>
2022-03-23 14:39:34 +0700

    Initial Commit

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
+
+    void sayHello(String paramName){
+        System.out.println("Hello " + paramName + ", My Name is " + name);
+    }
+}
