360b7ea46cc33c057506ec37594e88857a9bb8d3
miftahapriliandi <miftahapriliandi@gmail.com>
2022-03-24 15:13:40 +0700

    Update Method Overriding

--- src/Manager.java
+++ src/Manager.java
@@ -2,6 +2,6 @@ class Manager {
     String name;
 
     void sayHello(String name){
-        System.out.println("Hi " + name + ", My Name is " + this.name);
+        System.out.println("Hi " + name + ", My Name is Manager " + this.name);
     }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,4 +1,7 @@
 class VicePresident extends Manager{
     
+    void sayHello(String name){
+        System.out.println("Hi " + name + ", My Name is VP " + this.name);
+    }
 
 }
