42411770fb54441ec384ae1a8e129148cf1772b3
miftahapriliandi <miftahapriliandi@gmail.com>
2022-03-28 07:52:17 +0700

    Update Class Object

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -7,5 +7,10 @@ public class ManagerApp {
         var vp = new VicePresident("Reza");
 
         vp.sayHello("Alvin");
+
+        System.out.println(manager);
+        System.out.println(manager.toString());
+        System.out.println(vp);
+        System.out.println(vp.toString());
     }
 }
