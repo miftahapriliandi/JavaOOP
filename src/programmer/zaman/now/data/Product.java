9fa7dd465eb8596bfb5121fb129533468dc0aacd
miftahapriliandi <miftahapriliandi@gmail.com>
2022-03-30 07:22:08 +0700

    Update Package

---
+++ src/programmer/zaman/now/data/Product.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.data;
+
+class Product {
+
+    String name;
+    int price;
+
+    Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+}
